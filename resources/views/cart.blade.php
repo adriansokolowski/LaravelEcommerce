4c9540b0ab61990b5094ef2db48f26926ef2d6a6
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-09-20 15:09:42 +0200

    Display cart items

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,6 +4,20 @@
 {{ session()->get('success_message') }}
 @endif
 
+@if (Cart::count() > 0)
+{{ Cart::count() }} items in shopping cart
+<hr>
+@foreach(Cart::content() as $item)
+<div>{{ $item->model->name }}</div>
+<div>{{ $item->model->details }}</div>
+
+<a href="{{ route('shop.show', $item->model->slug) }}"><img width="100" src="{{ asset('img/products/product.jpg') }}"></img></a>
+@endforeach
+
+@else
+    No items in cart!
+@endif
+
 @if (count($errors) > 0)
 
 @foreach($errors->all as $error)
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\ShopController;
@@ -10,3 +11,7 @@ Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 Route::get('/shop/{product}', [ShopController::class, 'show'])->name('shop.show');
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
+
+// Route::get('/empty', function(){
+//     Cart::destroy();
+// });
