62d9df7b4912aaa52a51d9a62c7e9597f414b6e6
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-09-20 02:25:20 +0200

    Add landing page controller and return data

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel eCommerce</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    @foreach ($products as $product)
+    {{ $product->name }} <br>
+    @endforeach
+</body>
+
+</html>
