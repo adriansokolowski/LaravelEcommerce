57b237e38ef6196fa5c1fafddc20bf567b49df6c
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-09-20 02:08:34 +0200

    Add Product model with migration and factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'slug' => $this->faker->slug,
+            'details' => $this->faker->text(100),
+            'price' => $this->faker->numberBetween(100000, 999999),
+            'description' => $this->faker->text(200)
+        ];
+    }
+}
